233def70c9cbcc56a81e8d94abfdef5b3ff31c7a
mluigi <louis.1995.11.04@gmail.com>
2019-10-24 16:50:22 +0200

    Added CommodityAdapter to show market

--- app/src/main/java/m/luigi/eliteboy/StationFragment.kt
+++ app/src/main/java/m/luigi/eliteboy/StationFragment.kt
@@ -11,28 +11,54 @@ import kotlinx.android.synthetic.main.fragment_station.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import m.luigi.eliteboy.elitedangerous.adapters.CommodityAdapter
 import m.luigi.eliteboy.elitedangerous.adapters.InformationAdapter
 import m.luigi.eliteboy.elitedangerous.edsm.EDSMApi
 import m.luigi.eliteboy.elitedangerous.edsm.data.Station
 import m.luigi.eliteboy.util.onIO
 import m.luigi.eliteboy.util.onMain
+import m.luigi.eliteboy.util.setAnimateOnClickListener
 
 
 class StationFragment : Fragment() {
     lateinit var station: Station
     lateinit var getStationJob: Job
 
+    var isInfoOpened = true
+    var isMarketOpened = false
+    var isShipyardOpened = false
+    var isOutfittingOpened = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getStationJob = GlobalScope.launch {
             arguments?.let {
+
+
                 station = it.getParcelable("station") as Station
                 onMain {
                     stationLayout.visibility = View.GONE
                     stationSpinKit.visibility = View.VISIBLE
                     (activity as MainActivity).mainToolbar.title = station.name
+
+                    if (station.haveMarket) {
+                        onIO {
+                            EDSMApi.getMarket(station)
+                        }
+                    }
+
+                    if (station.haveShipyard) {
+                        onIO {
+                            EDSMApi.getShipyard(station)
+                        }
+                    }
+
+                    if (station.haveMarket) {
+                        onIO {
+                            EDSMApi.getOutfitting(station)
+                        }
+                    }
                 }
-                onIO { EDSMApi.getCompleteStation(station) }
             }
         }
 
@@ -53,11 +79,42 @@ class StationFragment : Fragment() {
             getStationJob.join()
 
             onMain {
-                stationLayout.visibility = View.VISIBLE
-                stationSpinKit.visibility = View.GONE
+
                 infoList.layoutManager =
                     LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
                 infoList.adapter = InformationAdapter(station.asMap(), view.context)
+
+                infoLayout.setAnimateOnClickListener(
+                    infoList,
+                    infoImg,
+                    { isInfoOpened }) { isInfoOpened = !isInfoOpened }
+
+                if (station.haveMarket) {
+                    marketLayout.setAnimateOnClickListener(
+                        marketList,
+                        marketImg,
+                        { isMarketOpened }) { isMarketOpened = !isMarketOpened }
+
+                    marketList.layoutManager =
+                        LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+                    marketList.adapter=CommodityAdapter(station.commodities!!,view.context)
+                } else {
+                    marketCardView.visibility = View.GONE
+                }
+
+                if (station.haveShipyard) {
+
+                } else {
+                    shipsCardView.visibility = View.GONE
+                }
+
+                if (station.haveOutfitting) {
+                } else {
+                    outfittingCardView.visibility = View.GONE
+                }
+
+                stationLayout.visibility = View.VISIBLE
+                stationSpinKit.visibility = View.GONE
             }
         }
     }
---
+++ app/src/main/java/m/luigi/eliteboy/elitedangerous/adapters/CommodityAdapter.kt
@@ -0,0 +1,108 @@
+package m.luigi.eliteboy.elitedangerous.adapters
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.commodities_item.view.*
+import m.luigi.eliteboy.R
+import m.luigi.eliteboy.elitedangerous.companionapi.data.Commodity
+import java.text.NumberFormat
+
+
+class CommodityAdapter(val commodities: ArrayList<Commodity>, val context: Context) :
+    RecyclerView.Adapter<CommodityAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.commodities_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return commodities.size + 1
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (position == 0) {
+            holder.name.text = "Name"
+
+            with(holder.buySell) {
+                val buy = SpannableString("Buy")
+                val sell = SpannableString("Sell")
+                buy.setSpan(
+                    ForegroundColorSpan(Color.GREEN),
+                    0,
+                    buy.length,
+                    SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                sell.setSpan(
+                    ForegroundColorSpan(Color.RED),
+                    0,
+                    sell.length,
+                    SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+
+                text = buy
+                append("/")
+                append(sell)
+            }
+            holder.demandStock.text = "Demand/Stock"
+            holder.buySell.setTypeface(holder.buySell.typeface, Typeface.BOLD)
+            holder.name.setTypeface(holder.name.typeface, Typeface.BOLD)
+            holder.demandStock.setTypeface(holder.demandStock.typeface, Typeface.BOLD)
+        } else {
+            with(commodities[position - 1]) {
+                holder.name.text = name
+
+                val nFormat = NumberFormat.getIntegerInstance()
+
+                if (buyPrice!! > sellPrice!!) {
+                    val buy = SpannableString(nFormat.format(buyPrice))
+                    buy.setSpan(
+                        ForegroundColorSpan(Color.GREEN),
+                        0,
+                        buy.length,
+                        SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    holder.buySell.text = buy
+                    holder.demandStock.text = nFormat.format(stock)
+                } else {
+                    val sell = SpannableString(nFormat.format(sellPrice))
+                    sell.setSpan(
+                        ForegroundColorSpan(Color.RED),
+                        0,
+                        sell.length,
+                        SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    holder.buySell.text = sell
+                    holder.demandStock.text = nFormat.format(demand)
+                }
+
+
+                holder.buySell.typeface = null
+                holder.name.typeface = null
+                holder.demandStock.typeface = null
+            }
+        }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name = view.nameView
+        val buySell = view.buySellView
+        val demandStock = view.demandStockView
+
+    }
+
+}
